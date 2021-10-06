0f6d285dfa98eda44b43633e6e8199b795eb322e
youngkwon02 <youngkwon02@naver.com>
2021-10-06 20:28:08 +0900

    refactor: modify given code

--- Assignment-01/flatten.py
+++ Assignment-01/flatten.py
@@ -10,7 +10,7 @@ def new_flatten(lst):
       if index > pointer:
         index = 0
         pointer += 1
-        return [e]
+        return e
     elif type(e) == list:
       if index + len(e) > pointer:
         return new_flatten(e)
@@ -18,17 +18,16 @@ def new_flatten(lst):
         index += len(e)
 
 
-
 # Add up numbers until the sum becomes greater than 10
 
-data = [1, [2, [[[4]]], [0, 5], [], 3], [4], 2, 7]
-# sum = 0
-# for e in new_flatten(data):
-#   print(e)
-#   sum += e
-#   if sum > 10:
-#     break
-# print('Result:', sum)
-
-for o in range(0, 10):
-  print('Result:', new_flatten(data))
+data = [1, [-2, [[[4]]], [0, 5], [], 3], [4], 2, 7]
+sum = 0
+while True:
+  e = new_flatten(data)
+  if e == None:
+    break
+  print(e)
+  sum += e
+  if sum > 10:
+    break 
+print('Result:', sum)
